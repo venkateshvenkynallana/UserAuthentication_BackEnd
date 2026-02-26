51acd1e698fcaea6ce88ac51be9ca49e65a3f50c
venkateshvenkynallana <venkateshnallana2003@email.com>
2026-02-26 19:06:29 +0530

    port

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Access at: http://localhost:${PORT}`);
 });
